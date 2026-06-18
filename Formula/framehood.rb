61aa8e34ec21b0cf6a93b3cfbdc07d40feedb185
Kirill Shidenko <kshidenko@gmail.com>
2026-06-18 11:09:38 +0300

    framehood 0.1.2

--- Formula/framehood.rb
+++ Formula/framehood.rb
@@ -1,27 +1,27 @@
 class Framehood < Formula
   desc "Generate images, video and audio with Framehood from the terminal"
   homepage "https://framehood.ai"
-  version "0.1.1"
+  version "0.1.2"
 
   on_macos do
     on_arm do
-      url "https://github.com/Framehood/framehood-cli/releases/download/v0.1.1/framehood_darwin_arm64.tar.gz"
-      sha256 "8a70144a60674a7bec3e49d5ba7442a078417956f1280174c1598e9775ddb52b"
+      url "https://github.com/Framehood/framehood-cli/releases/download/v0.1.2/framehood_darwin_arm64.tar.gz"
+      sha256 "28b691a361c2d437876ffdee92b52afb7350d75eaee06daab677acfbdb95ae9b"
     end
     on_intel do
-      url "https://github.com/Framehood/framehood-cli/releases/download/v0.1.1/framehood_darwin_amd64.tar.gz"
-      sha256 "890cd7131fe8260af445f8291a6bfb40e0a87b6190671d968e9289b3795a36f2"
+      url "https://github.com/Framehood/framehood-cli/releases/download/v0.1.2/framehood_darwin_amd64.tar.gz"
+      sha256 "26f513e6520d7789f4d04053dfd66ecfa08e58138683ffda96558148db4ece54"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/Framehood/framehood-cli/releases/download/v0.1.1/framehood_linux_arm64.tar.gz"
-      sha256 "93ced686c31744829059bfb82f1d8b342c7e5ba27a2ccbf6a33ded3e41f11cf1"
+      url "https://github.com/Framehood/framehood-cli/releases/download/v0.1.2/framehood_linux_arm64.tar.gz"
+      sha256 "99e18435aaa9d9a8d946570f618f45bbebb87774cd29f56bc7b3d10bd3d7c1d9"
     end
     on_intel do
-      url "https://github.com/Framehood/framehood-cli/releases/download/v0.1.1/framehood_linux_amd64.tar.gz"
-      sha256 "99be22096f93d36786cb95d13413f8676d67a6c6c74525214ee13cb2e3c726fd"
+      url "https://github.com/Framehood/framehood-cli/releases/download/v0.1.2/framehood_linux_amd64.tar.gz"
+      sha256 "503b59336a2791bd8f7b567d5d1f077d9cd0b7d55b8f948b9e588575bcac3dde"
     end
   end
 
